ebf10c079827a49b148a4e59435b5a1e8b669299
MarroosSK <maros1chmel@gmail.com>
2024-06-22 13:25:08 +0200

    3 - removed old components

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,3 @@
-//images
-
-// fonts
 import { FAQSection } from "@/components/sections/faq-section";
 import { FeaturesSection } from "@/components/sections/features-section";
 import { HeroSection } from "@/components/sections/hero-section";
@@ -8,16 +5,6 @@ import { LatestEditsSection } from "@/components/sections/latest-edits";
 import PricingSection from "@/components/sections/pricing-section";
 import { StatsSection } from "@/components/sections/stats-section";
 import { TestimonialsSection } from "@/components/sections/testimonials-section";
-// components
-// import Accordion from "@/components/accordion/accordion";
-// import CTA from "@/components/cta/CTA";
-// import Demos from "@/components/demos/demos";
-// import FAQ from "@/components/faq/faq";
-// import Features from "@/components/features/features";
-// import Hero from "@/components/hero/hero";
-// import Logos from "@/components/logos/logos";
-// import Pricing from "@/components/pricing/pricing";
-// import Testimonials from "@/components/testimonials/testimonials";
 
 const App = () => {
   return (
@@ -29,15 +16,6 @@ const App = () => {
       <PricingSection />
       <TestimonialsSection />
       <FAQSection />
-      {/* <Logos />
-      <Demos />
-      <Features />
-      <Pricing />
-      <Testimonials />
-      <FAQ>
-        <Accordion />
-      </FAQ>
-      <CTA /> */}
     </main>
   );
 };
